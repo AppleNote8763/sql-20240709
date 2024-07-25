4c472eb06d6640ef6404539f3436688ff6e824bb
AppleNote <a7201492425@gmail.com>
2024-07-25 13:56:44 +0900

    2024.07.25. 13:50

---
+++ Group_By.sql
@@ -0,0 +1,123 @@
+USE practice_sql;
+
+CREATE TABLE sale (
+	sequence_number INT PRIMARY KEY AUTO_INCREMENT,
+    date DATE,
+    amount INT,
+    employee_number INT
+);
+
+INSERT INTO sale (date, amount, employee_number)
+VALUES ('2024-07-01', 100000, 1);
+INSERT INTO sale (date, amount, employee_number)
+VALUES ('2024-07-01', 120000, 2);
+INSERT INTO sale (date, amount, employee_number)
+VALUES ('2024-07-01', 60000, 1);
+INSERT INTO sale (date, amount, employee_number)
+VALUES ('2024-07-03', 200000, 1);
+INSERT INTO sale (date, amount, employee_number)
+VALUES ('2024-07-03', 170000, 3);
+INSERT INTO sale (date, amount, employee_number)
+VALUES ('2024-07-06', 100000, 2);
+INSERT INTO sale (date, amount, employee_number)
+VALUES ('2024-07-06', 160000, 3);
+INSERT INTO sale (date, amount, employee_number)
+VALUES ('2024-07-07', 80000, 1);
+INSERT INTO sale (date, amount, employee_number)
+VALUES ('2024-07-08', 90000, 2);
+INSERT INTO sale (date, amount, employee_number)
+VALUES ('2024-07-08', 110000, 3);
+
+SELECT * FROM sale;
+
+-- 집계 함수 : 여러 행의 레코드를 종합하여 하나의 결과값을 반환
+
+-- COUNT() : 특정 조건에 해당하는 레코드의 개수를 반환
+SELECT COUNT(*) FROM sale;	-- 전체 레코드 개수
+SELECT COUNT(*) FROM sale WHERE amount <= 100000;	-- amount의 값이 100000보다 작은 개수
+SELECT COUNT(amount) FROM sale;
+
+-- SUM() : 특정 조건에 해당하는 컬럼의 값을 모두 더한 결과를 반환
+SELECT SUM(amount) FROM sale;
+SELECT SUM(amount) FROM sale WHERE employee_number = 1;
+
+-- AVG() : 특정 조건에 해당하는 컬럼의 값의 평균을 반환
+SELECT AVG(amount) FROM sale;
+SELECT AVG(amount) FROM sale WHERE employee_number = 1;
+
+-- MAX(), MIN() : 특정 조건에 해당하는 컬럼 값의 최대 최소 값을 반환
+SELECT MAX(amount), MIN(amount) FROM sale;
+
+
+-- 그룹화 (GROUP BY) : 조회의 결과에 대해 레코드를 하나 이상의 컬럼으로 그룹화하여 결과를 도출하는 것
+-- 일반적으로 집계함수와 함께 사용됨
+-- amount의 평균을 employee_number기준으로 그룹화
+SELECT AVG(amount), employee_number
+FROM sale
+GROUP BY employee_number;
+
+-- GROUP BY사용하는 employee_number를 SELECT문에서 사용하지 않아도 결과는 나온다
+SELECT AVG(amount)
+FROM sale
+GROUP BY employee_number;
+
+-- 집계함수가 포함되어 있는 쿼리 혹은 그룹화가 되어있는 쿼리에서는
+-- 그룹화한 컬럼과 집계함수를 제외한 컬럼은 SELECT 절에서 사용이 불가능
+-- 실행 안됨
+SELECT AVG(amount), date
+FROM sale
+GROUP BY employee_number;
+
+-- 2개 이상의 그룹화를 이용한 집계
+SELECT AVG(amount), employee_number, date
+FROM sale
+GROUP BY employee_number, date;
+
+-- SELECT DISTINCT와 같은 출력
+SELECT employee_number, date
+FROM sale
+GROUP BY employee_number, date;
+
+
+-- 필터링 (HAVING) : 그룹화된 결과에 필터 작업을 수행
+-- 주의!! WHERE 절과 사용방법이 비슷하지만 WHERE절은 조회할 때 사용, HAVING절은 조회 결과에 사용
+-- 실행 안됨
+SELECT
+	AVG(amount) '평균',
+    employee_number '사번'
+FROM sale
+WHERE AVG(amount) <= 100000
+GROUP BY employee_number;
+
+-- 실행가능
+SELECT
+	AVG(amount) '평균',
+    employee_number '사번'
+FROM sale
+GROUP BY employee_number
+HAVING AVG(amount) <= 110000;
+-- 실행가능
+SELECT
+	AVG(amount) '평균',
+    employee_number '사번'
+FROM sale
+GROUP BY employee_number
+HAVING 평균 <= 110000;
+
+-- 결과 테이블에 존재하지 않는 컬럼은 HAVING 절에서 사용할 수 없음
+-- 실행 안됨
+SELECT
+	AVG(amount) '평균',
+    employee_number '사번'
+FROM sale
+GROUP BY employee_number
+HAVING date < '2024-07-05';	-- date는 조회한 결과에 대한 값이 아니기 때문에 불가능
+
+-- 실행가능
+SELECT
+	AVG(amount) '평균',
+    employee_number '사번'
+FROM sale
+WHERE date < '2024-07-05'
+GROUP BY employee_number;
+
--- Join_Sub.sql
+++ Join_Sub.sql
@@ -41,6 +41,7 @@ VALUES ('이도', '17', 'D');
 SELECT * FROM employee;
 SELECT * FROM department;
 
+
 -- Alias : 쿼리문에서 사용되는 별칭
 -- 컬럼 및 테이블에서 사용가능
 -- 사용하는 이름을 변경하고 싶을때 적용
@@ -56,6 +57,7 @@ SELECT
     dpt.name '부서명',
     dpt.tel_number '부서 전화번호'
 FROM department dpt;
+
 
 -- JOIN : 두 개 이상의 테이블을 특정 조건에 따라 조합하여 결과를 조회하고자 할때 사용하는 명령어
 
@@ -149,4 +151,110 @@ SELECT
 FROM  employee E INNER JOIN department D
 ON E.department_code = D.department_code
 WHERE D.name = '영업부';
+-- 위의 조건에서의 조회 방식은 조회량이 많아 지기 때문에 서브쿼리를 이용하는게 좋음
 
+
+-- 서브쿼리 : 쿼리 내부에 존재하는 또 다른 쿼리, 쿼리 결과를 조건이나 테이블로 사용할 수 있도록 함
+
+-- WHERE 절에서 서브쿼리 : 조회 결과를 조건으로 사용하여 조건을 동적으로 지정할 수 있도록 함
+-- WHERE 절에서 비교 연산등으로 사용할 때 조회하는 컬럼의 개수 및 레코드의 개수 주의
+SELECT employee_number, name, age
+FROM employee
+WHERE department_code = (
+	SELECT department_code
+    FROM department
+    WHERE name = '영업부'
+);
+
+-- 실행안됨
+SELECT employee_number, name, age
+FROM employee
+WHERE department_code = (
+	SELECT *	-- *를 쓸려면 WHERE에서 해당 테이블에 모든 컬럼을 조건으로 걸어야함
+    FROM department
+    WHERE name = '영업부'
+);
+
+-- 실행안됨
+SELECT employee_number, name, age
+FROM employee
+WHERE department_code = (
+	SELECT department_code
+    FROM department
+);
+
+-- 실행됨
+SELECT employee_number, name, age
+FROM employee
+WHERE department_code IN (
+	SELECT department_code
+    FROM department
+);
+
+-- 실행안됨
+SELECT employee_number, name, age
+FROM employee
+WHERE department_code IN (
+	SELECT *
+    FROM department
+);
+
+
+-- 부서명이 '영업부'인 사원에 대해
+-- 사번, 이름, 나이를 조회하시오.
+-- 어디서 찾을지 잘 모르겠을때 이런식으로 해보자
+
+SELECT *
+FROM department
+WHERE name = '영업부';
+
+SELECT department_code
+FROM department
+WHERE name = '영업부';
+
+SELECT *
+FROM employee
+WHERE department_code = (
+	SELECT department_code
+    FROM department
+    WHERE name = '영업부'
+);
+
+SELECT employee_number, name, age
+FROM employee
+WHERE department_code = (
+	SELECT department_code
+    FROM department
+    WHERE name = '영업부'
+);
+
+
+-- FROM 절에서 서브쿼리 : 조회 결과 테이블을 다시 FROM 절에서 재사용
+SELECT * FROM department
+WHERE name = '영업부';
+
+SELECT
+	E.employee_number '사번',
+	E.name '이름',
+    E.age '나이'
+FROM  employee E INNER JOIN (
+	SELECT * FROM department
+	WHERE name = '영업부'
+    ) D
+ON E.department_code = D.department_code
+WHERE D.name = '영업부';
+
+-- 서브쿼리를 FROM 절에서 사용할 땐 반드시 별칭을 사용해야함
+-- 별칭이 없어서 실행안됨
+SELECT *
+FROM (
+	SELECT * FROM department
+)
+WHERE name = '영업부';
+
+-- 별칭이 있어서 실행가능
+SELECT *
+FROM (
+	SELECT * FROM department
+) D
+WHERE name = '영업부';
